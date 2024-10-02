02fe3d1992794cd0cf6c1330e2132263e27bbe4b
ali sadeghi <145838787+sdgali@users.noreply.github.com>
2024-10-02 17:50:13 +0300

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -64,7 +64,7 @@ def registration(request):
         login(request, user)
         data = {"userName": username, "status": "Authenticated"}
         return JsonResponse(data)
-        else:
+    else:
         data = {"userName": username, "error": "Already Registered"}
         return JsonResponse(data)
 
