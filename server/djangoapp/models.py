521cd4562ce507bbf185781f8be8fc082e077971
ali sadeghi <145838787+sdgali@users.noreply.github.com>
2024-10-02 18:40:30 +0300

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -40,7 +40,8 @@ class CarModel(models.Model):
         # Add more choices as required
     ]
     type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
-    year = models.IntegerField(default=2023, validators=[MaxValueValidator(2023), MinValueValidator(2015)])
+    year = models.IntegerField(
+    default=2023,validators=[MaxValueValidator(2023), MinValueValidator(2015)])
 
 def __str__(self):
     return self.name  # Return the name as the string representation
